cc9c239adef37ffd6905d342294d761e9c21816a
Raquel2023 <mallorca2005@yahoo.es>
2023-10-25 23:31:15 +0200

    Añadir Ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,12 @@
+const nombre = "Raquel"
+const edad = 50
+const esEstudiante = true
+const mensaje = "Hola, mi nombre es" + nombre + "y tengo" + edad
++ "años. Soy estudiante: " + esEstudiante + " "
+
+console.log ("Mensaje: ", mensaje)
+
+
+
+const num1 = 20
+const num2 = 85
