7c2fbc588e12bb904b42ffda3ade008b134a4ba3
mallorca2023 <mallorca2005@yahoo.es>
2023-11-13 09:16:39 +0100

    Añadir ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,12 +1,20 @@
-const nombre = "Raquel"
-const edad = 50
-const esEstudiante = true
-const mensaje = "Hola, mi nombre es" + nombre + "y tengo" + edad
-+ "años. Soy estudiante: " + esEstudiante + " "
+
+let nombre = "Raquel";
+let edad = 50
+let esEstudiante = true
+let mensaje = "Hola, mi nombre es " + nombre + " y tengo " + edad
++ " años. Soy estudiante: " + esEstudiante + " "
+
+let num1 = 25
+let num2 = 40
+
+let mult = num1 * num2
+let suma = num1 + num2
+let resta = num1 - num2
+let división = num1 / num2
 
 console.log ("Mensaje: ", mensaje)
-
-
-
-const num1 = 20
-const num2 = 85
+console.log ("Multiplicación: ", mult)
+console.log ("Suma: ", suma)
+console.log ("Resta: ", resta)
+console.log ("División: ", división)
